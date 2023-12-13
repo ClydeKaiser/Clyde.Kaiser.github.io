a11db993824b9cb70d97482e92f69a06628ed108
LukeSmelzer <77809246+LukeSmelzer@users.noreply.github.com>
2023-12-13 14:33:39 -0800

    Delete dist directory

--- dist/script.js
+++
@@ -1,7 +0,0 @@
-var amount = "";
-document.getElementById("betBtn").addEventListener("click", function money(){
-  amount = prompt("Enter the amount you would like to Bet", "$00.00")
-  if (amount != ""){
-  alert("Your Bet Has Been Placed! Thank you for your Buisness!");
-}
-});

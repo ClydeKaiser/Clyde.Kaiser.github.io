2878176fc1b07f9588c67b1175888b0c081da59e
LukeSmelzer <77809246+LukeSmelzer@users.noreply.github.com>
2023-12-13 23:08:06 -0800

    Update script.js

--- fight1/script.js
+++ fight1/script.js
@@ -3,5 +3,5 @@ document.getElementById("betBtn1").addEventListener("click", function fight1(){
   amount = prompt("Enter the amount you would like to Bet", "$00.00")
   if (fight1Amount != null && amount != "$00.00"){
   alert("Your bet has been placed! Thank you for your buisnesss!")
-}
+  }
 });
